6fe3d89a47bc0d711f997c3c4d58f33dfbfb23c5
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2022-12-31 01:29:45 +0200

    add base for editor
    mostly broken...
    a lot of stuff is wrong

--- src/utils/gamestate.gd
+++ src/utils/gamestate.gd
@@ -2,3 +2,17 @@ extends Node
 
 var sname: String
 var spath: String
+
+var texture_atlas: AtlasTexture = null 
+var cell_size: Vector2
+
+func get_texture_in_atlas(id: int):
+	var size = texture_atlas.get_size() / cell_size
+	if (id >= size.x * size.y):
+		return null
+	var pos := Vector2.ZERO
+	pos.y = floor(id / int(size.x))
+	pos.x = id % int(size.x)
+	var texture = texture_atlas.duplicate()
+	texture.region = Rect2(pos.x * cell_size.x, pos.y * cell_size.y, cell_size.x, cell_size.y)
+	return texture
--- src/wfc/tile_editor/TileEditor.gd
+++ src/wfc/tile_editor/TileEditor.gd
@@ -6,13 +6,29 @@ class PlacedTile:
 	var id: int
 	var x: int = 0
 	var y: int = 0
-	var node: MeshInstance2D
+	var scale: int = 1
+	var node: TextureRect
 	
-	func _init(x, y, scale, texture):
-		pass
+	func _init(x, y, scale, texture_id):
+		id = texture_id
+		self.x = x
+		self.y = y
+		self.scale = scale
+		node = TextureRect.new()
+		node.expand = true
+		node.rect_min_size = Vector2(1, 1)
+		node.rect_size = Vector2(1, 1)
+		node.rect_scale = Vector2(scale, scale)
+		node.rect_global_position = Vector2(x, y) * scale
+		node.texture = Gamestate.get_texture_in_atlas(texture_id)
+	
+	func redraw():
+		node.texture = Gamestate.get_texture_in_atlas(id)
 
-var tile_size = 16.0
-var scale = 1.0
+onready var tile_cont = $HSplitContainer/Tiles
+
+# var tile_size = 1.0
+var scale = 32.0
 
 var placed_tiles_array = []
 
@@ -30,19 +46,31 @@ var is_pixel_art: bool = true
 
 var texture_file: ImageTexture = null 
 # ^ used to be of type StreamTexture
-var texture_atlas: AtlasTexture = null 
 
 var hframes: int = 1
 var vframes: int = 1
-var cell_size: Vector2
 
-var brush: int = 0
+var brush: int = -1
 
 # Main
 
 func _ready():
 #	set_process(false)
 	pass
+
+func paint(canvas_pos: Vector2, offset: Vector2):
+	if (brush == -1):
+		return
+	var p = Vector2(floor(canvas_pos.x / scale), floor(canvas_pos.y / scale))
+	# add offset
+	for pt in placed_tiles_array:
+		if (pt.x == p.x && pt.y == p.y):
+			pt.id = brush
+			pt.redraw()
+			return
+	var pt = PlacedTile.new(p.x, p.y, scale, brush)
+	placed_tiles_array.append(pt)
+	tile_cont.add_child(pt.node)
 
 func _process(delta):
 	if (iter_tiles):
@@ -56,7 +84,7 @@ func fill_tile_selector():
 		child.queue_free()
 	
 	for i in range(0, int(hframes * vframes)):
-		var t: Texture = get_texture_in_atlas(i)
+		var t: Texture = Gamestate.get_texture_in_atlas(i)
 		var t_btn: TextureButton = TextureButton.new()
 		t_btn.expand = true
 		t_btn.stretch_mode = TextureButton.STRETCH_KEEP_ASPECT_CENTERED
@@ -64,19 +92,6 @@ func fill_tile_selector():
 		t_btn.texture_normal = t
 		tile_selector_cont.add_child(t_btn)
 		t_btn.connect("pressed", self, "_st_pressed", [i])
-
-# Local Utils
-
-func get_texture_in_atlas(id: int):
-	var size = texture_atlas.get_size() / cell_size
-	if (id >= size.x * size.y):
-		return null
-	var pos := Vector2.ZERO
-	pos.y = floor(id / int(size.x))
-	pos.x = id % int(size.x)
-	var texture = texture_atlas.duplicate()
-	texture.region = Rect2(pos.x * cell_size.x, pos.y * cell_size.y, cell_size.x, cell_size.y)
-	return texture
 
 # Signals
 
@@ -99,27 +114,27 @@ func _on_FileDialog_file_selected(path):
 	t.create_from_image(img, 1)
 	
 	texture_file = t
-	texture_atlas = AtlasTexture.new()
-	texture_atlas.atlas = texture_file
-	cell_size.x = texture_atlas.get_size().x / vframes
-	cell_size.y = texture_atlas.get_size().y / hframes
+	Gamestate.texture_atlas = AtlasTexture.new()
+	Gamestate.texture_atlas.atlas = texture_file
+	Gamestate.cell_size.x = Gamestate.texture_atlas.get_size().x / vframes
+	Gamestate.cell_size.y = Gamestate.texture_atlas.get_size().y / hframes
 	fill_tile_selector()
 
 func _on_HSpinBox_value_changed(value):
 	hframes = value
-	if (texture_atlas != null):
-		cell_size.y = texture_atlas.get_size().y / hframes
+	if (Gamestate.texture_atlas != null):
+		Gamestate.cell_size.y = Gamestate.texture_atlas.get_size().y / hframes
 		fill_tile_selector()
 
 func _on_VSpinBox_value_changed(value):
 	vframes = value
-	if (texture_atlas != null):
-		cell_size.x = texture_atlas.get_size().x / vframes
+	if (Gamestate.texture_atlas != null):
+		Gamestate.cell_size.x = Gamestate.texture_atlas.get_size().x / vframes
 		fill_tile_selector()
 
 func _st_pressed(id: int):
 	brush = id
-	brush_tr.texture = get_texture_in_atlas(id)
+	brush_tr.texture = Gamestate.get_texture_in_atlas(id)
 
 #func _on_IsPixelArt_toggled(button_pressed):
 #	is_pixel_art = button_pressed
